07c9305b4b128d1a6841a9542fbfadbd7fbe4b17
Rakesh Kumar Sharma <rakeshsharma715078@gmail.com>
2025-06-11 10:53:47 +0530

    after new server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,14 @@
+// export const environment = {
+//   production: true,
+//   baseurl: "https://api.scratchtosuccess.com/api",
+//   image_url: "https://api.scratchtosuccess.com",
+//   // frontend: "https://api.scratchtosuccess.com/api"
+// };
+
+
 export const environment = {
   production: true,
-  baseurl: "https://api.scratchtosuccess.com/api",
-  image_url: "https://api.scratchtosuccess.com",
-  // frontend: "https://api.scratchtosuccess.com/api"
+  baseurl: "https://scratchtosuccess.com/api",
+  image_url: "https://scratchtosuccess.com/api",
+  // frontend: "https://scratchtosuccess.com/api"
 };
