b6d9e3c8bf778e2fe51c44f60d2c88a22d6fea2e
Rakesh Kumar Sharma <rakeshsharma715078@gmail.com>
2025-06-13 15:14:42 +0530

    phonepe user testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component'; //
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },              // Load without changing URL
-  { path: ':lang', component: LandingPageComponent }          // Accept /en, /hi etc.
+  { path: ':lang', component: LandingPageComponent } ,
+  { path: 'payment-status', component: LandingPageComponent },        // Accept /en, /hi etc.
 ];
 
 @NgModule({
